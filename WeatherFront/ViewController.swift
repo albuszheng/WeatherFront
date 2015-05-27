7395cba9fe1c842741ce8a98089b77250cc781f7
Albus Zheng <albuszheng@hotmail.com>
2015-05-27 17:21:13 +0800

    Initial Commit

---
+++ WeatherFront/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherFront
+//
+//  Created by 轲阳 郑 on 5/27/15.
+//  Copyright (c) 2015 轲阳 郑. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

9ab36a1f54fe806a51ab7e48f46fa09d16b1a91a
Albus Zheng <albuszheng@hotmail.com>
2015-06-15 11:35:21 +0800

    change weather display view

--- WeatherFront/DestinationUIViewController.swift
+++ WeatherFront/DestinationUIViewController.swift
@@ -13,30 +13,11 @@ class DestinationWeatherUIViewController: UIViewController {
     var destinationCity: String = ""
 
     @IBOutlet weak var destinationName: UILabel!
-    
-    
-    @IBOutlet weak var presentDayDay: UILabel!
-    @IBOutlet weak var presentDayDate: UILabel!
-    @IBOutlet weak var presentDayWeatherText: UILabel!
-    @IBOutlet weak var presentDayLowestTemp: UILabel!
-    @IBOutlet weak var presentDayHighestTemp: UILabel!
-    @IBOutlet weak var presentDayWeatherLogo: UIImageView!
-    
-    
-    @IBOutlet weak var tomorrowDay: UILabel!
-    @IBOutlet weak var tomorrowDate: UILabel!
-    @IBOutlet weak var tomorrowWeatherText: UILabel!
-    @IBOutlet weak var tomorrowLowestTemp: UILabel!
-    @IBOutlet weak var tomorrowHighestTemp: UILabel!
-    @IBOutlet weak var tomorrowWeatherLogo: UIImageView!
-    
-    
-    @IBOutlet weak var theDayAfterDay: UILabel!
-    @IBOutlet weak var theDayAfterDate: UILabel!
-    @IBOutlet weak var theDayAfterWeatherText: UILabel!
-    @IBOutlet weak var theDayAfterLowestTemp: UILabel!
-    @IBOutlet weak var theDayAfterHighestTemp: UILabel!
-    @IBOutlet weak var theDayAfterWeatherLogo: UIImageView!
+    @IBOutlet weak var weatherImageMain: UIImageView!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var tempLow: UILabel!
+    @IBOutlet weak var tempHigh: UILabel!
+    @IBOutlet weak var weatherCondition: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,25 +31,7 @@ class DestinationWeatherUIViewController: UIViewController {
     }
     
     private func updateUIInfo(data: WeatherData){
-        destinationName.text = data.cityShowName
-        
-        presentDayDay.text = data.presentDay.day
-        presentDayDate.text = data.presentDay.date
-        presentDayHighestTemp.text = data.presentDay.HighestTemp
-        presentDayLowestTemp.text = data.presentDay.lowestTemp
-        presentDayWeatherText.text = data.presentDay.weatherText
-        
-        tomorrowDay.text = data.tomorrow.day
-        tomorrowDate.text = data.tomorrow.date
-        tomorrowHighestTemp.text = data.tomorrow.HighestTemp
-        tomorrowLowestTemp.text = data.tomorrow.lowestTemp
-        tomorrowWeatherText.text = data.tomorrow.weatherText
-        
-        theDayAfterDay.text = data.theDayAfter.day
-        theDayAfterDate.text = data.theDayAfter.date
-        theDayAfterHighestTemp.text = data.theDayAfter.HighestTemp
-        theDayAfterLowestTemp.text = data.theDayAfter.lowestTemp
-        theDayAfterWeatherText.text = data.theDayAfter.weatherText
+
     }
     
     
